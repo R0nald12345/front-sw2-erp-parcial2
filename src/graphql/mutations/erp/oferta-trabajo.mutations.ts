f06fd62c968511b03567d032849753cecc9adeb8
Mikel Ken <mayki6866@gmail.com>
2025-11-10 21:12:34 -0400

    offers by company

--- src/graphql/mutations/erp/oferta-trabajo.mutations.ts
+++ src/graphql/mutations/erp/oferta-trabajo.mutations.ts
@@ -1,33 +1,36 @@
 export const CREAR_OFERTA_TRABAJO = `
-  mutation CreateOfertaTrabajo(
-    $titulo: String!
-    $descripcion: String!
-    $salario: Float!
-    $ubicacion: String!
-    $requisitos: String!
-    $fechaPublicacion: String!
-    $empresaId: String!
-  ) {
-    createOfertaTrabajo(
-      titulo: $titulo
-      descripcion: $descripcion
-      salario: $salario
-      ubicacion: $ubicacion
-      requisitos: $requisitos
-      fechaPublicacion: $fechaPublicacion
-      empresaId: $empresaId
-    ) {
-      id
-      titulo
-      descripcion
-      salario
-      empresa {
-        id
-        nombre
+ mutation CreateOferta(
+        $titulo: String!
+        $descripcion: String!
+        $salario: Float
+        $ubicacion: String
+        $requisitos: String
+        $fechaPublicacion: String
+        $empresaId: UUID!
+      ) {
+        createOfertaTrabajo(
+          titulo: $titulo
+          descripcion: $descripcion
+          salario: $salario
+          ubicacion: $ubicacion
+          requisitos: $requisitos
+          fechaPublicacion: $fechaPublicacion
+          empresaId: $empresaId
+        ) {
+          id
+          titulo
+          descripcion
+          salario
+          ubicacion
+          requisitos
+          fechaPublicacion
+          createdAt
+          empresa {
+            id
+            nombre
+          }
+        }
       }
-      createdAt
-    }
-  }
 `;
 
 export const ACTUALIZAR_OFERTA_TRABAJO = `
